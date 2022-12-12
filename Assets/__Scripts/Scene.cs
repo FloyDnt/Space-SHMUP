dc99aeacce5802871c2a9cebd0c6ee8a2cf6a090
Nikita <56556577+FloyDnt@users.noreply.github.com>
2022-12-12 16:55:15 +0300

    Space-SHMUP v1.0
    Space-SHMUP ver1.0
    - Game menu
    - Game

---
+++ Assets/__Scripts/Scene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public void SceneChanger(string _sceneName)
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
